64a6a0a95a2015bfab553eef5dedffeaf1cc2d01
unknown <jreidmke@gmail.com>
2020-10-21 15:05:46 -0500

    new routing for invoices

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 /** BizTime express application. */
-
-
 const express = require("express");
-
 const app = express();
 const ExpressError = require("./expressError")
-
-const cRoutes = require('./routes/companies');
-app.use('/companies', cRoutes );
-
 app.use(express.json());
 
+const cRoutes = require('./routes/companies');
+app.use("/companies", cRoutes);
+
+const iRoutes = require('./routes/invoices');
+app.use("/invoices", iRoutes);
 
 /** 404 handler */
 
