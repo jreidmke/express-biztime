78e7bcbe628a4be2fa2eb2c11f69cddcbd977ec9
unknown <jreidmke@gmail.com>
2020-10-21 15:07:34 -0500

    new CRUD routing for invoices and companies

--- routes/companies.js
+++ routes/companies.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = ('../db');
+const db = require('../db');
 
 router.get('/', async(req, res, next) => {
     try {
@@ -10,3 +10,43 @@ router.get('/', async(req, res, next) => {
         next(error);
     }
 })
+
+router.get('/:code', async(req, res, next) => {
+    try {
+        const results = await db.query(`SELECT * FROM companies WHERE code=$1`, [req.params.code]);
+        return res.json(results.rows);
+    } catch (error) {
+        next(error);
+    }
+})
+
+router.post('/', async(req, res, next) => {
+    try {
+        const { code, name, description } = req.body;
+        const results = await db.query(`INSERT INTO companies (code, name, description) VALUES($1, $2, $3) RETURNING *`, [code, name, description]);
+        return res.status(201).json(results.rows);
+    } catch (error) {
+        next(error);
+    }
+})
+
+router.patch('/:code', async(req, res, next) => {
+    try {
+        const {name, description} = req.body;
+        const results = await db.query(`UPDATE companies SET name=$1, description=$2 WHERE code=$3 RETURNING *`, [name, description, req.params.code]);
+        return res.json(results.rows);
+    } catch (error) {
+        next(error);
+    }
+})
+
+router.delete('/:code', async(req, res, next) => {
+    try {
+        const results = await db.query(`DELETE FROM companies WHERE code=$1 RETURNING 'Company Deleted'`, [req.params.code]);
+        return res.json(results.rows);
+    } catch (error) {
+        next(error);
+    }
+})
+
+module.exports = router;
---
+++ routes/invoices.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const router = express.Router();
+const db = require('../db');
+
+router.get('/', async(req, res, next) => {
+    try {
+        const results = await db.query(`SELECT * FROM invoices`);
+        return res.json(results.rows);
+    } catch (error) {
+        next(error);
+    }
+})
+
+router.get('/:id', async(req, res, next) => {
+    try {
+        const results = await db.query(`SELECT * FROM invoices INNER JOIN companies ON (comp_code = code) WHERE id=$1`, [req.params.id]);
+        return res.json(results.rows);
+    } catch (error) {
+        next(error);
+    }
+})
+
+router.post('/', async(req, res, next) => {
+    try {
+        const { comp_code, amt } = req.body;
+        const results = await db.query(`INSERT INTO invoices (comp_code, amt) VALUES($1, $2) RETURNING *`, [comp_code, amt]);
+
+        return res.status(201).json(results.rows);
+    } catch (error) {
+        next(error);
+    }
+})
+
+router.patch('/:id', async(req, res, next) => {
+    try {
+        const { amt } = req.body;
+        const results = await db.query(`UPDATE invoices SET amt=$1 WHERE id=$2 RETURNING *`, [amt, req.params.id]);
+        return res.json(results.rows);
+    } catch (error) {
+        next(error);
+    }
+})
+
+router.delete('/:id', async(req, res, next) => {
+    try {
+        const results = await db.query(`DELETE FROM invoices WHERE id=$1 RETURNING 'Invoice Deleted'`, [req.params.id]);
+        return res.json(results.rows);
+    } catch (error) {
+        next(error);
+    }
+})
+
+module.exports = router;
