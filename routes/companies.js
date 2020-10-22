63efba81f29263ed3340b3768f73eeecaaa4c556
unknown <jreidmke@gmail.com>
2020-10-22 15:45:26 -0500

    cleaned up patch route

--- routes/companies.js
+++ routes/companies.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const db = require('../db');
+const ExpressError = require("../expressError");
 
 router.get('/', async(req, res, next) => {
     try {
@@ -14,6 +15,9 @@ router.get('/', async(req, res, next) => {
 router.get('/:code', async(req, res, next) => {
     try {
         const results = await db.query(`SELECT * FROM companies WHERE code=$1`, [req.params.code]);
+        if(results.rows.length === 0) {
+            throw new ExpressError(`Can't find user with id ${req.params.id}`, 404)
+        }
         return res.json(results.rows);
     } catch (error) {
         next(error);
@@ -32,9 +36,9 @@ router.post('/', async(req, res, next) => {
 
 router.patch('/:code', async(req, res, next) => {
     try {
-        const {name, description} = req.body;
-        const results = await db.query(`UPDATE companies SET name=$1, description=$2 WHERE code=$3 RETURNING *`, [name, description, req.params.code]);
-        return res.json(results.rows);
+        const {name, description, code} = req.body;
+        const results = await db.query(`UPDATE companies SET name=$1, description=$2, code=$4 WHERE code=$3 RETURNING *`, [name, description, req.params.code, code]);
+        return res.json(results.rows[0]);
     } catch (error) {
         next(error);
     }
